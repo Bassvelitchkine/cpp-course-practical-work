a3558a2ac2429a2f761c34316cd4d808579ba9ca
Bastiche <bastien.velitchkine@live.fr>
2021-10-20 11:55:45 +0200

    Created addition and multiplication methods

--- tp1/Number.hpp
+++ tp1/Number.hpp
@@ -17,7 +17,7 @@ class Number
 public:
     Number(unsigned long l) : first_{new Digit{l}}
     {
-        std::cout << "Number::Number : " << l << "\n";
+        //std::cout << "Number::Number : " << l << "\n";
     }
 
     ~Number()
@@ -30,6 +30,16 @@ public:
         this->first_->print(out);
     }
 
+    void add(unsigned int n)
+    {
+        this->first_->addition(n);
+    }
+
+    void multiply(unsigned int n)
+    {
+        this->first_->multiplication(n, 0);
+    }
+
 private:
     using DigitType = unsigned int;
     // Un seul chiffre décimal par maillon : l'objectif ici n'est pas la performance
@@ -40,7 +50,7 @@ private:
         Digit(unsigned long l)
             : digit_{static_cast<unsigned int>(l % number_base)}, next_{l < number_base ? nullptr : new Digit{l / number_base}}
         {
-            std::cout << "Digit::Digit : " << l << "\n";
+            //std::cout << "Digit::Digit : " << l << "\n";
         }
         // Then a destructor for the structure
         ~Digit()
@@ -49,13 +59,51 @@ private:
             {
                 delete next_;
             }
-            std::cout << "Digit::~Digit : " << digit_ << "\n";
+            //std::cout << "Digit::~Digit : " << digit_ << "\n";
         }
         // A printer for the structure
         void print(std::ostream &out) const
         {
             if (next_)
                 this->next_->print(out);
+            out << digit_;
+        }
+        // A few calculations
+        void addition(unsigned int n)
+        {
+            unsigned int temp{this->digit_ + n};
+            if (temp < number_base)
+            {
+                digit_ = temp;
+            }
+            else
+            {
+                digit_ = temp % number_base;
+                if (next_)
+                {
+                    next_->addition(temp / number_base);
+                }
+                else
+                {
+                    next_ = new Digit(temp / number_base);
+                }
+            }
+        }
+        void multiplication(unsigned int n, unsigned int retainer)
+        {
+            unsigned int temp{this->digit_ * n + retainer};
+            digit_ = temp % number_base;
+            if (next_)
+            {
+                next_->multiplication(n, temp / number_base);
+            }
+            else
+            {
+                if (temp >= number_base)
+                {
+                    next_ = new Digit(temp / number_base);
+                }
+            }
         }
         // Below are the Digit structure attributes
         DigitType digit_;
--- tp1/TestNumber.cpp
+++ tp1/TestNumber.cpp
@@ -22,11 +22,33 @@ void test22()
 {
     Number *myNumber = new Number(243);
     std::cout << *myNumber << "\n";
-    operator<<(std::cout, *myNumber);
     delete myNumber;
+}
+
+void test31()
+{
+    Number myNumber{99};
+    Number secondNumber{4};
+    // Addition
+    myNumber.add(7);
+    myNumber.print(std::cout);
+    std::cout << "\n";
+    myNumber.add(3);
+    myNumber.print(std::cout);
+    std::cout << "\n";
+    // Multiplication
+    secondNumber.multiply(2);
+    secondNumber.print(std::cout);
+    std::cout << "\n";
+    secondNumber.multiply(5);
+    secondNumber.print(std::cout);
+    std::cout << "\n";
+    secondNumber.multiply(101);
+    secondNumber.print(std::cout);
+    std::cout << "\n";
 }
 
 int main()
 {
-    test22();
+    test31();
 }
