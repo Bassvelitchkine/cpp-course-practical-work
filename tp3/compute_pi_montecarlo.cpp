f27956caba5ea96df979cc1ed899cd2ee0b1a9c6
Bastiche <bastien.velitchkine@live.fr>
2021-11-29 10:12:02 +0100

    Implemented pi computation with montecarlo

--- tp3/compute_pi_montecarlo.cpp
+++ tp3/compute_pi_montecarlo.cpp
@@ -10,7 +10,7 @@
 #include <future>
 #include <iostream>
 #include <iomanip>
-#include <numbers>
+// #include <numbers>
 #include <random>
 #include <string>
 #include <thread>
@@ -18,3 +18,49 @@
 #include <cctype>
 #include <cmath>
 #include <cstdlib>
+
+using FloatKind = float;
+const FloatKind pi{3.14159265358979323846};
+
+FloatKind next_random()
+{
+    static thread_local std::mt19937 generator{std::random_device{}()};
+    std::uniform_real_distribution<FloatKind> distribution{};
+    return distribution(generator);
+};
+
+unsigned long nb_points_inside_unit_circle(unsigned long nb_points)
+{
+    FloatKind random_x{};
+    FloatKind random_y{};
+    unsigned long result = 0;
+    for (unsigned long i = 0; i < nb_points; i++)
+    {
+        random_x = next_random();
+        random_y = next_random();
+        if (random_x * random_x + random_y * random_y <= 1)
+        {
+            result++;
+        };
+    };
+    return result;
+};
+
+FloatKind compute_pi(unsigned long nb_points)
+{
+    FloatKind total = static_cast<FloatKind>(nb_points);
+    FloatKind inside_circle = static_cast<FloatKind>(nb_points_inside_unit_circle(nb_points));
+    return 4 * inside_circle / total;
+}
+
+void test_pi_computation(unsigned long nb_points)
+{
+    FloatKind error{std::abs(pi - compute_pi(nb_points))};
+    std::cout << "Error is: " << error << "\n\n";
+}
+
+int main(int argc, char *argv[])
+{
+    unsigned long nb_points = std::stoull(argv[1]);
+    test_pi_computation(nb_points);
+};
