63e9919d920e31983bcac1d29b1dfee7461a914c
Bastiche <bastien.velitchkine@live.fr>
2021-11-29 11:42:15 +0100

    Implemented multithreaded computations

--- tp3/compute_pi_montecarlo.cpp
+++ tp3/compute_pi_montecarlo.cpp
@@ -19,7 +19,7 @@
 #include <cmath>
 #include <cstdlib>
 
-using FloatKind = long double;
+using FloatKind = float;
 const FloatKind pi{3.14159265358979323846};
 
 FloatKind next_random()
@@ -46,72 +46,106 @@ unsigned long nb_points_inside_unit_circle(unsigned long nb_points)
     return result;
 };
 
-FloatKind compute_pi(unsigned long nb_points)
+// FloatKind compute_pi(unsigned long nb_points)
+// {
+//     FloatKind total = static_cast<FloatKind>(nb_points);
+//     FloatKind inside_circle = static_cast<FloatKind>(nb_points_inside_unit_circle(nb_points));
+//     return 4 * inside_circle / total;
+// }
+
+// //test11
+// void test_pi_computation(unsigned long nb_points, unsigned int nb_threads)
+// {
+//     FloatKind error{std::abs(pi - compute_pi(nb_points, nb_threads))};
+//     std::cout << "Error is: " << error << "\n\n";
+// }
+
+// //test12
+
+// // FLOAT:
+// // With nb_points = 70000000 and -o, execution time is ~22s.
+// // With nb_points = 70000000 and -O, execution time is ~12s.
+// // With nb_points = 70000000 and -O2, execution time is ~10s.
+// // With nb_points = 70000000 and -O3, execution time is ~10s.
+
+// // DOUBLE:
+// // With nb_points = 70000000 and -o, execution time is ~25s.
+
+// // LONG DOUBLE:
+// // With nb_points = 70000000 and -o, execution time is ~33s.
+// void test_execution_time(unsigned long nb_points)
+// {
+//     auto start = std::chrono::high_resolution_clock::now();
+//     FloatKind error{std::abs(pi - compute_pi(nb_points))};
+//     auto end = std::chrono::high_resolution_clock::now();
+//     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+//     std::cout << "Error is: " << error << "\n\n";
+//     std::cout << "Execution time is: " << duration.count() << "ms\n\n";
+// }
+
+// //test14
+
+// // FLOAT:
+// // Max variability is ~10^-2
+
+// // DOUBLE:
+// // Max variability is ~10^-2
+
+// // LONG DOUBLE:
+// // Max variability still is ~10^-2
+// void test_variability(unsigned long nb_points)
+// {
+//     FloatKind greatest_difference{0};
+//     FloatKind res{};
+//     for (int i = 0; i < 10; i++)
+//     {
+//         res = std::abs(pi - compute_pi(nb_points));
+//         if (res > greatest_difference)
+//         {
+//             greatest_difference = res;
+//         }
+//     }
+//     std::cout << "Greatest estimation error is: " << greatest_difference << "\n\n";
+// }
+
+FloatKind compute_pi(unsigned long nb_points, unsigned int nb_threads)
 {
+    std::vector<std::thread> pool{};
+    std::vector<std::future<unsigned long>> results{};
+    for (int i = 0; i < nb_threads; i++)
+    {
+        std::packaged_task<unsigned long(unsigned long)> task(nb_points_inside_unit_circle);
+        results.push_back(task.get_future());
+
+        pool.push_back(std::thread(std::move(task), nb_points / nb_threads));
+    }
+
+    unsigned long points_inside = 0;
+    for (int i = 0; i < nb_threads; i++)
+    {
+        pool[i].join();
+        points_inside = points_inside + results[i].get();
+    }
+
+    FloatKind inside_circle = static_cast<FloatKind>(points_inside);
     FloatKind total = static_cast<FloatKind>(nb_points);
-    FloatKind inside_circle = static_cast<FloatKind>(nb_points_inside_unit_circle(nb_points));
     return 4 * inside_circle / total;
 }
 
-//test11
-void test_pi_computation(unsigned long nb_points)
+// test2
+// With nb_points = 70000000 and -o, execution time is ~10s, much faster than single-threaded computations
+void test_multithreaded_pi_computation(unsigned long nb_points, unsigned int nb_threads)
 {
-    FloatKind error{std::abs(pi - compute_pi(nb_points))};
+    FloatKind error{std::abs(pi - compute_pi(nb_points, nb_threads))};
     std::cout << "Error is: " << error << "\n\n";
-}
-
-//test12
-
-// FLOAT:
-// With nb_points = 70000000 and -o, execution time is ~22s.
-// With nb_points = 70000000 and -O, execution time is ~12s.
-// With nb_points = 70000000 and -O2, execution time is ~10s.
-// With nb_points = 70000000 and -O3, execution time is ~10s.
-
-// DOUBLE:
-// With nb_points = 70000000 and -o, execution time is ~25s.
-
-// LONG DOUBLE:
-// With nb_points = 70000000 and -o, execution time is ~33s.
-void test_execution_time(unsigned long nb_points)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    FloatKind error{std::abs(pi - compute_pi(nb_points))};
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Error is: " << error << "\n\n";
-    std::cout << "Execution time is: " << duration.count() << "ms\n\n";
-}
-
-//test14
-
-// FLOAT:
-// Max variability is ~10^-2
-
-// DOUBLE:
-// Max variability is ~10^-2
-
-// LONG DOUBLE:
-// Max variability still is ~10^-2
-void test_variability(unsigned long nb_points)
-{
-    FloatKind greatest_difference{0};
-    FloatKind res{};
-    for (int i = 0; i < 10; i++)
-    {
-        res = std::abs(pi - compute_pi(nb_points));
-        if (res > greatest_difference)
-        {
-            greatest_difference = res;
-        }
-    }
-    std::cout << "Greatest estimation error is: " << greatest_difference << "\n\n";
 }
 
 int main(int argc, char *argv[])
 {
     unsigned long nb_points = std::stoull(argv[1]);
+    unsigned int nb_threads = std::stoull(argv[2]);
     // test_pi_computation(nb_points);
     // test_execution_time(nb_points);
     // test_variability(nb_points);
+    test_multithreaded_pi_computation(nb_points, nb_threads);
 };
